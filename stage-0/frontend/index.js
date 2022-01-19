63aec10f7b31c5c7ed381893d97811afc354a476
Albert Bregonia <47435340+albertbregonia@users.noreply.github.com>
2022-01-19 18:43:55 -0500

    removed oop example as it was correct structurally but not functionally lol

--- stage-0/frontend/index.js
+++ stage-0/frontend/index.js
@@ -1,23 +1,5 @@
-class Rectangle {
-    constructor(length, width) {
-        this.length = length;
-        this.width = width;
-    }
-    get length() {
-        return this.length;
-    }
-
-    get width() {
-        return this.width;
-    }
-
-    set length(length) {
-        this.length = length; 
-    }
-
-    set width(width) {
-        this.width = width;
-    }
+const arr = [1,2,3,34,45,65,67,7,7,88,0];
+while(arr.length) {
+    console.log(arr[arr.length-1]);
+    arr.pop();
 }
-
-const rect = new Rectangle(5,7);
